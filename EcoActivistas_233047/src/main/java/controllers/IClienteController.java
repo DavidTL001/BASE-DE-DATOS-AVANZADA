732776f31f35b6c5270bf37de2177b86b4a2a77a
DavidTL001 <david.tirado233047@potros.itson.edu.mx>
2026-02-11 17:50:37 -0600

    Create IClienteController.java

---
+++ EcoActivistas_233047/src/main/java/controllers/IClienteController.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package controllers;
+
+/**
+ *
+ * @author David TL
+ */
+public interface IClienteController {
+    boolean agregar (String nombre, String direccion, String telefonos);
+    
+}
