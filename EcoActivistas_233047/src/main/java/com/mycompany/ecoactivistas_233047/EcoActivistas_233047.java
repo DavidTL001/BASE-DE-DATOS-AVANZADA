803f472c4a3fa7e18ea5106f345c2a7e52cb7728
DavidTL001 <david.tirado233047@potros.itson.edu.mx>
2026-02-11 17:50:35 -0600

    Create EcoActivistas_233047.java

---
+++ EcoActivistas_233047/src/main/java/com/mycompany/ecoactivistas_233047/EcoActivistas_233047.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecoactivistas_233047;
+
+/**
+ *
+ * @author David TL
+ */
+public class EcoActivistas_233047 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
